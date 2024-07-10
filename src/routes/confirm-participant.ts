e17e62fe85f7ebe66d96ab188e2fc40c49571bbb
Artur Damaceno Abreu <arturabreu110@gmail.com>
2024-07-10 13:46:53 -0300

    first commit

---
+++ src/routes/confirm-participant.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function confirmParticipant(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/participants/:participantsId/confirm",
+    {
+      schema: {
+        params: z.object({
+          participantsId: z.string().uuid(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { participantsId } = request.params;
+
+      const participant = await prisma.participant.findUnique({
+        where: {
+          id: participantsId,
+        },
+      });
+
+      if (!participant) {
+        throw new Error("Participant not found");
+      }
+
+      if (participant.is_confirmed) {
+        return reply.redirect(
+          `https://localhost:3000/trips/${participant.trip_id}`
+        );
+      }
+
+      await prisma.participant.update({
+        where: {
+          id: participantsId,
+        },
+        data: {
+          is_confirmed: true,
+        },
+      });
+
+      return reply.redirect(
+        `https://localhost:3000/trips/${participant.trip_id}`
+      );
+    }
+  );
+}
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { createTrip } from "./routes/create-trip";
+import {
+  validatorCompiler,
+  serializerCompiler,
+} from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipant } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-acitivy";
+import { getActivities } from "./routes/get-acitivies";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+
+const app = fastify();
+
+app.register(cors, {
+  origin: "*"
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
+app.register(getParticipants);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("server Running");
+});
